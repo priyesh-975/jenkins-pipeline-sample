f7240ae1087518531124c2dec2b179b64c88b2d6
Priyesh <ps881220@gmail.com>
2021-05-30 16:54:08 +0530

    This is first commit

---
+++ src/main/java/com/example/project/PipeLineController.java
@@ -0,0 +1,15 @@
+package com.example.project;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PipeLineController {
+	
+	@GetMapping("/hello")
+	public String sayHello()
+	{
+		return "hello Priyesh";
+	}
+}
